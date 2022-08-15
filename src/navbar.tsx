4b24f1aeb9a5d42330e2602038e118065412e11f
David Everett <davideverett@davids-mbp.myfiosgateway.com>
2022-08-14 20:06:48 -0400

    team page

--- src/navbar.tsx
+++ src/navbar.tsx
@@ -22,8 +22,8 @@ function NavBar() {
         <div className="container flex flex-wrap mx-auto py-2 px-4 {hidden:!navbarOpen}">
           <a href='#' className='inline-flex p-2 text-white text-xl font-bold uppercase tracking-wider'>COD Stats</a>
           <button className='inline-flex items-center justify-center text-white border h-10 w-10 rounded-md outline-none foucs:outline-none lg:hidden ml-auto'  onClick={handleHover}>
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
             </svg>
           </button> 
           <div className={`w-full lg:inline-flex lg:w-auto mt-2 lg:mt-0 ${dropdown ? '' : 'hidden'}`}>
--- src/teams/team.tsx
+++ src/teams/team.tsx
@@ -7,11 +7,41 @@ import NavBar from "../navbar";
 export default function Team() {
     const [team, setTeam] = useState([])
     const [players, setPlayers] = useState([])
+    const [teamLocation, setTeamLocation] = useState('')
+    const [teamName, setTeamName] = useState('')
+    const [teamPhotoUrl, setTeamPhotoURL] = useState('')
+    const [message, setMessage] = useState('')
     
 
     const handleDelete = () => {
         fetch(`${process.env.REACT_APP_API_URL}/teams/${slug}`, { method: 'DELETE' })
     }
+
+    let handleSubmit = async (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+        try {
+          let res = await fetch(`${process.env.REACT_APP_API_URL}/team/${slug}`, {
+            method: "PATCH",
+            headers: {
+              'Content-Type': 'application/json; charset=utf-8'
+            },
+            body: JSON.stringify({
+            slug: slug,
+            teamLocation: teamLocation,
+              teamName: teamName,
+              teamPhotoURL: teamPhotoUrl,
+            }),
+          });
+          let resJson = await res.json();
+          if (res.status === 200 || res.status === 201) {
+            fetchData()
+          } else {
+            setMessage("Some error occured");
+          }
+        } catch (err) {
+          // console.log(err);
+        }
+      };
 
     const fetchData = () => {
         fetch(`${process.env.REACT_APP_API_URL}/teams/${slug}`)
@@ -20,8 +50,12 @@ export default function Team() {
           })
           .then(data => {
             setTeam(data)
+            setTeamName(data['teamName'])
+            setTeamLocation(data['teamLocation'])
+            setTeamPhotoURL(data['teamPhotoURL'])
           })
       }
+
     const fetchPlayers = () => {
         fetch(`${process.env.REACT_APP_API_URL}/players/team/${slug}`)
         .then(response => {
@@ -45,7 +79,26 @@ export default function Team() {
         <div>
             <NavBar />
             <h1>{team['teamLocation']} {team['teamName']}</h1>
-            <span><a href={"/teams"}>All Teams</a></span>
+            <h2>Update Team</h2>
+            <form onSubmit={handleSubmit}>
+                    <input
+                    type="text"
+                    value={teamLocation}
+                    placeholder={teamLocation}
+                    onChange={(e) => setTeamLocation(e.target.value)}/>
+                    <input
+                    type="text"
+                    value={teamName}
+                    placeholder={teamName}
+                    onChange={(e) => setTeamName(e.target.value)}/>
+                    <input
+                    type="text"
+                    value={teamPhotoUrl}
+                    placeholder={teamPhotoUrl}
+                    onChange={(e) => setTeamPhotoURL(e.target.value)}/>
+                    <button type="submit">Update</button>
+            </form>
+            <h2>Players</h2>
             <table style={{ width: 500 }}>
                 <thead>
                     <tr>
